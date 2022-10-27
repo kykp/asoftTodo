5d0db79d207ede383e02b7db5047704ff48df785
kykp <kykp@yandex.ru>
2022-10-27 18:49:05 +0400

    heroku

--- mongoconnect.ts
+++ mongoconnect.ts
@@ -4,8 +4,7 @@ dotenv.config()
 const mongoUrl =  process.env.MONGO__URI;
 
 async function startBaseMongo() {
-  const uri =
-    `mongodb+srv://kykp:VYFFvDBVwrRPnGHr@cluster0.xtwcw.mongodb.net/Users?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://kykp:VYFFvDBVwrRPnGHr@cluster0.xtwcw.mongodb.net/Users?retryWrites=true&w=majority`;
   try {
     console.log("успешное подключение к БД");
     mongoose.connect(uri);
